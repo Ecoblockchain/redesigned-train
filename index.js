c89855f239e079a6973e140473ad55d9f014508f
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2016-03-01 21:47:12 +0100

    attempts at optimization and queueing (stack used instead of queue though)

--- index.js
+++ index.js
@@ -13,21 +13,26 @@ var client = new Twitter({
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
+var stack = [];
 
 //client.stream('user', {}, (stream) => { //this uses your timeline instead but needs consumer_key and other stuff up above
 client.stream('statuses/filter', {track: process.env.VR_TRACK || '#gamedev'}, (stream) => {
   stream.on('data', (tweet) => {
-    if (!("user" in tweet || "entities" in tweet)) return;
-    console.log(tweet);
+    if (stack.length > 1000) { console.log("maximum capacity reached"); return; }
+    if (!("user" in tweet && "entities" in tweet)) return;
+    if (!("media" in tweet.entities)) return;
     var payload = {};
     payload.source = "twitter";
     payload.main_image = tweet.user.profile_image_url;
     payload.media = [];
     if ("media" in tweet.entities) for (var media of tweet.entities.media) payload.media.push(media.media_url);
-    io.emit('tweet', payload);
+    stack.push(payload);
   });
   stream.on('error', (error) => {
     throw error;
   });
 });
-srv.listen(4000);
+setInterval(() => {
+  if (stack.length > 0) io.emit("tweet", stack.pop());
+}, process.env.VR_EMIT_INTERVAL || 100);
+srv.listen(process.env.VR_PORT || 4000);
